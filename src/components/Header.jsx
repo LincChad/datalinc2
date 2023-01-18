a80c6d9395a001003aa842eff156f1de74581b52
Linc Chad <linc@MacBook-Pro.local>
2023-01-19 05:26:43 +1100

    Improved header, started with MUI components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
+import { AppBar, Toolbar, Button } from "@mui/material";
 import React from "react";
+import "../App.css"
 
 function Header() {
-    return (<div className="App-header">
-        <h1>
-            <code>Datalinc</code>
-        </h1>
-        </div>
-    )
+
+    const displayDesktop = () => {
+        return <Toolbar className="App-header"><code>Datalinc</code><Button variant="contained">Sign Up</Button></Toolbar>
+    }
+  
+    return (
+        <header>
+            <AppBar>{displayDesktop()}</AppBar>
+        </header>
+    );
 }
 export default Header;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ function Hero() {
   return ( <div className='App-hero'>
         <img src={logo} className="App-logo" alt="Datalinc Logo" />
         <h1>Welcome to <code>Datalinc.</code></h1>
+        <h3>Everything from HTML to Web3.</h3>
         <p>Website coming soon.</p>
         </div>
   )
