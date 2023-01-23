2f319b475a07c0c89c47fe844beaf5ac429d9e12
Linc Chad <linc@MacBook-Pro.local>
2023-01-24 05:45:53 +1100

    Hero update, added columns

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,15 @@ import logo from '../datalinc.png'
 
 
 function Hero() {
-  return ( <div className='App-hero'>
+  return ( <div className='App-hero , container'>
+    <div className='column-1'>
         <img src={logo} className="App-logo" alt="Datalinc Logo" />
+      </div>
+      <div className='column-2'>
         <h1>Welcome to <code>Datalinc.</code></h1>
         <h3>Everything from HTML to Web3.</h3>
         <p>Website coming soon.</p>
+        </div>
         </div>
   )
 };
