cd4bbcef72212729d813c52168475558b543224b
Linc Chad <linc@MacBook-Pro.local>
2023-05-16 19:21:14 +1000

    Added a body, with about/vision/services

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Footer from './components/Footer';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Body />
       <Footer />
     </div>
   );
---
+++ src/components/Body.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Body() {
+    return ( <div className="App-body">
+            <h1>About Datalinc</h1>
+                <p> Datalinc is a cutting-edge technology company dedicated to empowering businesses with the integration of Artificial Intelligence (AI) and Web3 solutions for continuous improvement. We specialize in harnessing the power of these transformative technologies to drive innovation, enhance operational efficiency, and unlock new possibilities for businesses in a rapidly evolving digital landscape.</p>
+            <h1>Our Vision</h1>
+                <p>At Datalinc, we envision a future where businesses can leverage AI and Web3 technologies to gain a competitive edge, streamline processes, and make data-driven decisions with confidence. We believe that the seamless integration of these powerful tools can revolutionize industries and pave the way for unprecedented growth and success.</p>
+            <h1>Our Services</h1>
+                <ul>
+                    <li><strong>AI Integration:</strong> Our team of AI experts works closely with businesses to identify opportunities for integrating AI technologies into their operations. We offer customized AI solutions tailored to specific business needs, such as machine learning algorithms, predictive analytics, natural language processing, and computer vision. By harnessing the power of AI, businesses can automate processes, gain valuable insights, and optimize their operations for greater efficiency.</li>
+                    <li><strong>Web3 Integration:</strong> We help businesses embrace the potential of Web3 technologies, including blockchain and decentralized applications (dApps). Our expertise lies in guiding businesses through the process of integrating blockchain solutions into their existing systems, developing smart contracts, and leveraging the benefits of decentralized platforms. By incorporating Web3 technologies, businesses can enhance transparency, security, and efficiency in their processes while exploring new business models and revenue streams.</li>
+                    <li><strong>Consultancy and Strategy:</strong> Our team of experienced consultants works closely with businesses to understand their unique challenges and objectives. We provide strategic guidance, helping businesses identify the most suitable AI and Web3 solutions for their specific needs. We collaborate with our clients to develop comprehensive roadmaps, ensuring a smooth integration process and maximizing the potential of these technologies.</li>
+                    <li><strong>Training and Education:</strong> We are committed to fostering knowledge and empowering businesses to embrace AI and Web3 technologies. Through workshops, training programs, and educational resources, we equip businesses with the skills and knowledge necessary to leverage these technologies effectively. We aim to bridge the gap between technology and businesses, enabling our clients to stay ahead in the rapidly evolving digital landscape.</li>
+                </ul>
+                <p>At Datalinc, we are driven by a passion for innovation, a commitment to excellence, and a belief in the transformative power of AI and Web3 technologies. Partner with us to unlock new opportunities, drive growth, and achieve unparalleled success in the digital age. Together, let's embrace the future of business transformation.</p>
+        </div>
+    );
+
+};
+
+export default Body;
