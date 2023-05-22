078f3c6b14635610c7ed2144b0ba1b2194d6e05d
Linc Chad <linc@MacBook-Pro.local>
2023-05-22 11:27:01 +1000

    fixed hero to include DL globe

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../datalinc.png'
+import Logo from '../components/Logo/Logo.jsx'
+
 
 
 function Hero() {
   return ( <div className='App-hero'>
-        <img src={logo} className="App-logo" alt="Datalinc Logo" />
+        <div className='hero-logo'><Logo /></div>
         <h1>Welcome to <code>Datalinc.</code></h1>
         <h3>Everything from HTML to Web3.</h3>
         <p>Website coming soon.</p>
